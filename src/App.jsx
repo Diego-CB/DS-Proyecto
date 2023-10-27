de367cc42229685c2e52790ae58fd0da91dadd2b
caguirre5 <107177715+caguirre5@users.noreply.github.com>
2023-10-27 14:59:50 -0600

    Avances en pagina web

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,80 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Chart from 'chart.js/auto';
+import { BarChart } from '@mui/x-charts/BarChart';
 import './App.css'
+import { red } from '@mui/material/colors';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1>Proyecto Final</h1>
-      <div>
-        <aside>
+    <div className='app-container'>
+      {/* <header className="app-header">
+        <h2>Proyecto Final</h2>
+      </header> */}
+      <div className='flex-container'>
+        <aside className='aside-container'>
+          <div className='title-container'>
+            <h2>Start </h2>
+            <p>Lorem ipsum</p>
+          </div>
           <form onSubmit={{}}>
             <div>
-              <label>
-                Claim:
-                <input
-                  type="text"
-                  name="claim"
-                />
-              </label>
+              <textarea className='argument-input'
+                name="claim"
+                placeholder='Write your arguments...'
+                id="claim"
+                rows="4"  
+              ></textarea>
             </div>
-            <div>
+            <div className='claimtype-container'>
               <label>
                 Claim type:
-                <input
-                  type="text"
-                  name="claimType"
-                />
               </label>
+              <select
+                  name="selectedClaimType"
+                >
+                  <option value="Lead">Lead</option>
+                  <option value="Position">Position</option>
+                  <option value="Claim">Claim</option>
+                  <option value="Counterclaim">Counterclaim</option>
+                  <option value="Rebuttal">Rebuttal</option>
+                  <option value="Evidence">Evidence</option>
+                  <option value="Concluding Statement">Concluding Statement</option>
+              </select>
             </div>
-            <button type="submit">Enviar</button>
+            <div className='result-container'>
+              
+            </div>
+            <button className='evaluate-button' type="submit">Evaluate</button>
           </form>
         </aside>
+        <div className='graph-container'>
+          <div className='result-container'>
+            <h1>Result</h1>
+          </div>
+          <BarChart
+            xAxis={[
+              {
+                id: 'barCategories',
+                data: ['bar A', 'bar B', 'bar C'],
+                scaleType: 'band',
+              },
+            ]}
+            series={[
+              {
+                data: [2, 5, 3],
+              },
+            ]}
+            width={500}
+            height={300}
+            colors={red}
+          />
+        </div>
       </div>
-    </>
+    </div>
   )
 }
 
