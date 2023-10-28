f723f344fc113095d4006b43b5578d5837544ca5
Cristian Aguirre <107177715+caguirre5@users.noreply.github.com>
2023-10-27 20:52:00 -0600

    Avances

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,24 @@ import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Chart from 'chart.js/auto';
-import { BarChart } from '@mui/x-charts/BarChart';
+import { LineChart } from '@mui/x-charts/LineChart';
 import './App.css'
-import { red } from '@mui/material/colors';
+import { red, green, blue } from '@mui/material/colors';
+
+const customColors = [blue[500]]; // Define los colores personalizados aquí
+
 
 function App() {
   const [count, setCount] = useState(0)
+  const [selectedModelType, setSelectedModelType] = useState('RNN');
+
+  const handleClaimTypeChange = (event) => {
+    setSelectedModelType(event.target.value);
+  };
+  
+  const handleSubmit = (e) => {
+    console.log('Submit')
+  }
 
   return (
     <div className='app-container'>
@@ -16,12 +28,12 @@ function App() {
       </header> */}
       <div className='flex-container'>
         <aside className='aside-container'>
-          <div className='title-container'>
-            <h2>Start </h2>
-            <p>Lorem ipsum</p>
-          </div>
-          <form onSubmit={{}}>
-            <div>
+          
+          <form onSubmit={handleSubmit}>
+            <div className='title-container'>
+              <h2>Welcome to our text evaluation system! </h2>
+              <p>Please enter your written work in the box below and choose the type of text from the options provided. </p>
+              <div>
               <textarea className='argument-input'
                 name="claim"
                 placeholder='Write your arguments...'
@@ -45,33 +57,59 @@ function App() {
                   <option value="Concluding Statement">Concluding Statement</option>
               </select>
             </div>
-            <div className='result-container'>
-              
+            <div className='modeltype-container'>
+                <label>
+                  <input
+                    type="radio"
+                    name="selectedModelType"
+                    value="RNN"
+                    checked={selectedModelType === 'RNN'}
+                    onChange={handleClaimTypeChange}
+                  />
+                  RNN
+                </label>
+                <label>
+                  <input
+                    type="radio"
+                    name="selectedModelType"
+                    value="CNN"
+                    checked={selectedModelType === 'CNN'}
+                    onChange={handleClaimTypeChange}
+                  />
+                  CNN
+                </label>
+                <label>
+                  <input
+                    type="radio"
+                    name="selectedModelType"
+                    value="GPT"
+                    checked={selectedModelType === 'GPT'}
+                    onChange={handleClaimTypeChange}
+                  />
+                  GPT
+                </label>
+            </div>
             </div>
             <button className='evaluate-button' type="submit">Evaluate</button>
           </form>
         </aside>
-        <div className='graph-container'>
+        <div className='main-container'>
           <div className='result-container'>
-            <h1>Result</h1>
+            <h1>Result:</h1>
+            <h1>Effective</h1>
           </div>
-          <BarChart
-            xAxis={[
-              {
-                id: 'barCategories',
-                data: ['bar A', 'bar B', 'bar C'],
-                scaleType: 'band',
-              },
-            ]}
-            series={[
-              {
-                data: [2, 5, 3],
-              },
-            ]}
-            width={500}
-            height={300}
-            colors={red}
-          />
+          <div className='chart-component'>
+            <LineChart 
+              xAxis={[{ data: [1, 2, 3, 5, 8, 10] }]}
+              series={[
+                {
+                  data: [2, 5.5, 2, 8.5, 1.5, 5],
+                },
+              ]}
+              colors={customColors}
+            />
+          </div>
+          
         </div>
       </div>
     </div>
